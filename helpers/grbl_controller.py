cd9339743f46cb21ac4bf1ab5a5b7e506733fa6f
DanielWould <daniel@makergeek.co.uk>
2020-11-08 17:21:39 +0000

    try again

--- helpers/grbl_controller.py
+++ helpers/grbl_controller.py
@@ -81,7 +81,7 @@ class grbl_controller:
             self.grbl_connection.write('\n'.encode())
             status = self.grbl_connection.readline()
             count=0
-            while status != "ok" & status !="error":
+            while (status != "ok") and (status !="error"):
                 print("grbl:{} -  {}".format(time.ctime(),status))
                 count = count +1    
                 status = self.grbl_connection.readline()
