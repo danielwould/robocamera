9160edcc5e5df6ba4db16a4e761c0c9155c49074
DanielWould <daniel@makergeek.co.uk>
2020-11-08 17:34:34 +0000

    fix casing

--- helpers/grbl_controller.py
+++ helpers/grbl_controller.py
@@ -81,7 +81,7 @@ class grbl_controller:
             self.grbl_connection.write('\n'.encode())
             status = self.grbl_connection.readline().decode("utf-8")
             count=0
-            while (status.startsWith("ok") == False) or (status.startsWith("error")==False):
+            while (status.startswith("ok") == False) or (status.startswith("error")==False):
                 print("grbl:{} ->{}<-".format(time.ctime(),status))
                 count = count +1    
                 status = self.grbl_connection.readline().decode("utf-8")
