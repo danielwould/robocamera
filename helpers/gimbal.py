fdbf26c9b95fde9c71e066bc5def0d1d0cd06f11
DanielWould <daniel@makergeek.co.uk>
2020-11-08 15:41:04 +0000

    drop async from gimbal controls

--- helpers/gimbal.py
+++ helpers/gimbal.py
@@ -34,37 +34,37 @@ class gimbal(base_control_object):
         self.currentlocation.increment_zoom(self.big_step_zoom)
 
     # override move commands to include the third axis
-    async def move_to_position_at_rate(self, position):
+    def move_to_position_at_rate(self, position):
         self.controller.absolute_move(position.get_rotation_pos(), position.get_tilt_pos(), position.get_zoom_pos(),
                                       self.current_feed_speed,0)
         self.last_command_sent_at = time.time()
         self.currentlocation.set_location(position)
 
-    async def move_to_position_in_time(self, position):
+    def move_to_position_in_time(self, position):
         self.controller.absolute_move_by_time(position.get_rotation_pos(), position.get_tilt_pos(),
                                               position.get_zoom_pos(),
                                               self.current_move_duration,0)
         self.last_command_sent_at = time.time()
         self.currentlocation.set_location(position)
 
-    async def move_to_waypoint(self, position, feed_rate):
+    def move_to_waypoint(self, position, feed_rate):
         self.controller.absolute_move(position.get_rotation_pos(), position.get_tilt_pos(), position.get_zoom_pos(),
                                       feed_rate)
         self.last_command_sent_at = time.time()
         self.currentlocation.set_location(position)
         
-    async def move_to_waypoint_by_time(self, position, duration):
+    def move_to_waypoint_by_time(self, position, duration):
         self.controller.absolute_move_by_time(position.get_rotation_pos(),position.get_tilt_pos(), position.get_zoom_pos(),duration)
         self.last_command_sent_at = time.time()
         self.currentlocation.set_location(position)
 
-    async def add_waypoint_by_time_to_sequqnce(self, position, duration, dwell):
+    def add_waypoint_by_time_to_sequqnce(self, position, duration, dwell):
         self.controller.add_absolute_move_by_time_to_sequence(position.get_rotation_pos(),position.get_tilt_pos(),position.get_zoom_pos(),duration, dwell)
         self.last_position_in_sequence = position
         print("{} waypoint added to gcode sequence".format(time.ctime()))
 
 
-    async def add_waypoint_by_feedrate_to_sequqnce(self, position, duration, dwell):
+    def add_waypoint_by_feedrate_to_sequqnce(self, position, duration, dwell):
         self.controller.add_absolute_move_by_feed_to_sequence(position.get_rotation_pos(),position.get_tilt_pos(),position.get_zoom_pos(),duration, dwell)
         self.last_position_in_sequence = position
         print("{} waypoint added to gcode sequence".format(time.ctime()))
