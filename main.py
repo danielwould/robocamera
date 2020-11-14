a22f3ea330dd5a592df7444dcd8fa3e63096c409
DanielWould <daniel@makergeek.co.uk>
2020-11-14 17:17:09 +0000

    tick less often

--- main.py
+++ main.py
@@ -449,9 +449,12 @@ def main():
 
         # Get count of joysticks.
         joystick_count = pygame.joystick.get_count()
-        gimbal_inst.tick("Gimbal")
-        crane_inst.tick("Crane")
-        if time.time()-lastStatus >0.5:
+        if time.time()-lastStatus > 0.1:
+            gimbal_inst.tick("Gimbal")
+            crane_inst.tick("Crane")
+        
+        
+        if time.time()-lastStatus >0.8:
             gimbal_inst.status()
             crane_inst.status()
             lastStatus=time.time()
