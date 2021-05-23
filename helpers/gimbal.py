7da177078b9177f485632e36f6c24efc51c1c003
DanielWould <daniel@makergeek.co.uk>
2021-05-23 16:20:23 +0100

    clean up dead code

--- helpers/gimbal.py
+++ helpers/gimbal.py
@@ -31,33 +31,6 @@ class gimbal(base_control_object):
         self.last_command_sent_at = time.time()
 
     # override move commands to include the third axis
-    def move_to_position_at_rate(self, position):
-        self.controller.absolute_move(position.get_rotation_pos(), position.get_tilt_pos(), position.get_zoom_pos(),
-                                      0)
-        self.last_command_sent_at = time.time()
-
-    def move_to_position_in_time(self, position):
-        self.controller.absolute_move_by_time(position.get_rotation_pos(), position.get_tilt_pos(),
-                                              position.get_zoom_pos(),
-                                              0)
-        self.last_command_sent_at = time.time()
-
-    def move_to_waypoint(self, position, feed_rate):
-        self.controller.absolute_move(position.get_rotation_pos(), position.get_tilt_pos(), position.get_zoom_pos(),
-                                      feed_rate)
-        self.last_command_sent_at = time.time()
-        
-    def move_to_waypoint_by_time(self, position, duration):
-        self.controller.absolute_move_by_time(position.get_rotation_pos(),position.get_tilt_pos(), position.get_zoom_pos(),duration)
-        self.last_command_sent_at = time.time()
-
-    def add_waypoint_by_time_to_sequqnce(self, position, duration, dwell):
-        self.controller.add_absolute_move_by_time_to_sequence(position.get_rotation_pos(),position.get_tilt_pos(),position.get_zoom_pos(),duration, dwell)
-        print("{} waypoint added to gcode sequence".format(time.ctime()))
-
-
-    def add_waypoint_by_feedrate_to_sequqnce(self, position, duration, dwell):
-        self.controller.add_absolute_move_by_feed_to_sequence(position.get_rotation_pos(),position.get_tilt_pos(),position.get_zoom_pos(),duration, dwell)
-        print("{} waypoint added to gcode sequence".format(time.ctime()))
+    
 
  
