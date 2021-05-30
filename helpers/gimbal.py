f3179be1d160906c99160bfb6ff236ebdf34891a
Daniel Would <daniel@makergeek.co.uk>
2021-05-30 10:34:03 +0100

    update zoom commands to same relative move changes

--- helpers/gimbal.py
+++ helpers/gimbal.py
@@ -11,23 +11,25 @@ class gimbal(base_control_object):
         
 
     def zoom_in_small(self):
-        self.controller.relative_move("{}-{}".format(self.zoom_axis,
-            self.small_step_zoom))
+        if time.time()-self.last_command_sent_at > self.command_throttle_limit:
+            self.controller.relative_move(self.zoom_axis,0.2)
+          
         self.last_command_sent_at = time.time()
 
     def zoom_out_small(self):
-        self.controller.relative_move(
-            "{}{}".format(self.zoom_axis,self.small_step_zoom))
+
+        if time.time()-self.last_command_sent_at > self.command_throttle_limit:
+            self.controller.relative_move(self.zoom_axis,-0.2)
         self.last_command_sent_at = time.time()
 
     def zoom_in_large(self):
-        self.controller.relative_move("{}-{}".format(
-            self.zoom_axis,self.big_step_zoom))
+        if time.time()-self.last_command_sent_at > self.command_throttle_limit:
+            self.controller.relative_move(self.zoom_axis,0.5)
         self.last_command_sent_at = time.time()
 
     def zoom_out_large(self):
-        self.controller.relative_move(
-            "{}{}".format(self.zoom_axis,self.big_step_zoom))
+        if time.time()-self.last_command_sent_at > self.command_throttle_limit:
+            self.controller.relative_move(self.zoom_axis,0.5)
         self.last_command_sent_at = time.time()
 
     # override move commands to include the third axis
