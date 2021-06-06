21dee2b6388b364d009f320263be17f301e4047c
DanielWould <daniel@makergeek.co.uk>
2021-06-06 16:18:20 +0100

    check if data can be read of video stream for width height

--- helpers/tracking/aruco_tracker.py
+++ helpers/tracking/aruco_tracker.py
@@ -75,8 +75,8 @@ class aruco_tracker:
                             self.deltaX = (initialPositionX - trackedX)
                             self.deltaY = (initialPositionY - trackedY)
                         if (trackedId == 2):
-                            width  = image.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)   # float `width`
-                            height = image.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)  # float `height`
+                            width  = self.vs.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)   # float `width`
+                            height = self.vs.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)  # float `height`
                             #tracker 2 always centres
                             self.deltaX = ((width/2) - trackedX)
                             self.deltaY = ((height/2) - trackedY)
