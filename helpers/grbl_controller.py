d5c2268e864fbf3b7b7c44ea30fb74b7798451ad
DanielWould <daniel@makergeek.co.uk>
2021-11-06 13:57:21 +0000

    try setting coords with g10l20

--- helpers/grbl_controller.py
+++ helpers/grbl_controller.py
@@ -289,6 +289,7 @@ class grbl_controller:
 
     def set_position(self,x,y,z,a,b):
         self.queue.put("G92 X{} Y{} Z{} A{} B{}".format(x,y,z,a,b))
+        self.queue.put("G10 L20 P1 X{} Y{} Z{} A{} B{}".format(x,y,z,a,b))
         self.mcontrol.viewStatusReport()
         
     def stop(self):
